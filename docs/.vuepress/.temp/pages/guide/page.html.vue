e72246219b1f0c5db556feae095d28ddbebf1fe5
AndPuqing <1569540783@qq.com>
2021-09-20 23:20:40 +0800

    fix ignore error

--- docs/.vuepress/.temp/pages/guide/page.html.vue
+++
@@ -1,2 +0,0 @@
-<template><h1 id="asd" tabindex="-1"><a class="header-anchor" href="#asd" aria-hidden="true">#</a> asd</h1>
-</template>
--- docs/.vuepress/.temp/pages/study/algorithm/file0.html.vue
+++
@@ -1,7 +0,0 @@
-<template><blockquote>
-<p>记录写Oj过程中的小tips.</p>
-</blockquote>
-<h3 id="一、输入" tabindex="-1"><a class="header-anchor" href="#一、输入" aria-hidden="true">#</a> 一、输入</h3>
-<p>部分题目会要求多行输入，那可以采用如下方法：</p>
-<p><strong>有多个case输入，一行一个结果</strong></p>
-</template>
